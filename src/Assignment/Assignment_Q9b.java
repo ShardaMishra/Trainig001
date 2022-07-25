92276940a21a0e6aa05a1347cd01feb070b96e50
Sharda Mishra <shardamishra667@gmail.com>
2022-07-25 17:41:45 +0530

    Assignment submittion

---
+++ src/Assignment/Assignment_Q9b.java
@@ -0,0 +1,20 @@
+package Assignment;
+
+public class Assignment_Q9b {
+
+	public static void main(String[] args) {
+		
+		int num=8;
+		for(int a=1;a<=8;a++)
+		{
+			for(int b=8;b>=a;b--)
+			{
+				System.out.print("#");
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ src/Assignment/Assignment_Q9d.java
@@ -0,0 +1,26 @@
+package Assignment;
+
+public class Assignment_Q9d {
+
+	public static void main(String[] args) {
+		
+		int num=8;
+		for(int a=1;a<=8;a++)
+		{
+			for(int b=1;b<=8;b++)
+			{
+				if(a+b>=9)
+				{
+					System.out.print("#");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
