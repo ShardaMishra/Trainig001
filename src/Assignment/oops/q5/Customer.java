16c4da4b2ce506adc73eee33af8868307713476c
Sharda Mishra <102789200+ShardaMishra@users.noreply.github.com>
2022-08-01 18:37:43 +0530

    Update Customer.java

--- src/Assignment/oops/q5/Customer.java
+++ src/Assignment/oops/q5/Customer.java
@@ -2,10 +2,8 @@ package com.josh.java.training.assignment.oops.q5;
 
 import lombok.Data;
 
-//imported data class to implement setter and getter method
 @Data
 
-//this is the super class of staff class and student class
 public class Customer {
 
 	private String name;
