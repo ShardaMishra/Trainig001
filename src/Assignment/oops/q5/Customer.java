afb989728b7ce88f808ca2ebd9397b0d2e8f2fd3
Sharda Mishra <102789200+ShardaMishra@users.noreply.github.com>
2022-08-01 17:23:02 +0530

    Update Customer.java

--- src/Assignment/oops/q5/Customer.java
+++ src/Assignment/oops/q5/Customer.java
@@ -9,16 +9,10 @@ import lombok.Data;
 public class Customer {
 
 	private String name;
-	private boolean member = false;
+	private boolean member;
 	private String memberType;
 
 	public Customer(String name) {
 		this.name = name;
 	}
-
-	@Override
-	public String toString() {
-		return "Customer [name=" + name + ", member=" + member + ", memberType=" + memberType + "]";
-	}
-
 }
