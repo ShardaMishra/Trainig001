862b815f91901c6e8888370f10f6fb60fc08d8b2
Sharda Mishra <102789200+ShardaMishra@users.noreply.github.com>
2022-08-01 18:30:25 +0530

    Update Person.java

--- src/Assignment/oops/q2/Person.java
+++ src/Assignment/oops/q2/Person.java
@@ -3,17 +3,9 @@ package com.josh.java.training.assignment.oops.q2;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Person {
 	private String name;
 	private String address;
 
-	Person(String name, String address) {
-		this.name = name;
-		this.address = address;
-	}
-
-	@Override
-	public String toString() {
-		return "Person [name=" + name + ", address=" + address + "]";
-	}
 }
