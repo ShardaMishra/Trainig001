1f4864ec663350506d8e13991fe0ba3ecc89e1d9
Sharda Mishra <102789200+ShardaMishra@users.noreply.github.com>
2022-08-01 18:34:21 +0530

    Update Staff.java

--- src/Assignment/oops/q2/Staff.java
+++ src/Assignment/oops/q2/Staff.java
@@ -2,8 +2,8 @@ package com.josh.java.training.assignment.oops.q2;
 
 import lombok.Data;
 
-//imported data class to implement setter and getter method
 @Data
+@tostring(callsuper=true)
 public class Staff extends Person {
 	private String school;
 	private double pay;
@@ -13,10 +13,6 @@ public class Staff extends Person {
 		this.school = school;
 		this.pay = pay;
 	}
+}	
 
-	@Override
-	public String toString() {
-		return "Staff[Person [name=" + super.getName() + ", address=" + super.getAddress() + "[ name=" + getName()
-				+ ", address=" + getAddress() + ", school=" + school + ", pay=" + pay + " ]";
-	}
-}
+	
