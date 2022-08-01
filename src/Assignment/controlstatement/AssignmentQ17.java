3a736fd0af6243ba69b1da18fe7e37bb7137f421
Sharda Mishra <102789200+ShardaMishra@users.noreply.github.com>
2022-08-01 17:08:38 +0530

    Update AssignmentQ17.java

--- src/Assignment/controlstatement/AssignmentQ17.java
+++ src/Assignment/controlstatement/AssignmentQ17.java
@@ -3,36 +3,36 @@ package com.josh.java.training.assignment.controlstatement;
 
 class QuickSort
 {
-	public static void sort(int[] a,int start,int end)
+	public static void quickSort(int[] inputArray,int start,int end)
 	{
 		if(start>=end) return;
-		int pivot=a[(start+end)/2];
+		int pivot=inputArray[(start+end)/2];
 		int i=start;
 		int j=end;
 		while(i<=j)
 		{
-			while(a[i]<pivot) i++;
-			while(a[j]>pivot) j--;
+			while(inputArray[i]<pivot) i++;
+			while(inputArray[j]>pivot) j--;
 
-			int temp=a[i];
-			a[i]=a[j];
-			a[j]=temp;
+			int temp=inputArray[i];
+			inputArray[i]=inputArray[j];
+			inputArray[j]=temp;
 			i++;
 			j--;
 
 		}
 
-		sort(a,start,j);
-		sort(a,i,end);
+		quickSort(inputArray,start,j);
+		quickSort(inputArray,i,end);
 	}
 
 
 public class AssignmentQ17 {
 	public static void main(String[] args) {
 
-		int[] a= {5,3,8,9,1,2};
-		sort(a,0,a.length-1);
-		for(int n:a) System.out.println(n);
+		int[] inputArray= {5,3,8,9,1,2};
+		quickSort(inputArray,0,inputArray.length-1);
+		for(int n:inputArray) System.out.println(n);
 	}
 	}
 
