9abbd46189ea5176f66b644e8f0015ad1c270031
Sharda Mishra <102789200+ShardaMishra@users.noreply.github.com>
2022-08-01 18:27:12 +0530

    Update Person.java

--- src/Assignment/oops/q2/Person.java
+++ src/Assignment/oops/q2/Person.java
@@ -2,7 +2,6 @@ package com.josh.java.training.assignment.oops.q2;
 
 import lombok.Data;
 
-//imported data class to implement setter and getter method
 @Data
 public class Person {
 	private String name;
