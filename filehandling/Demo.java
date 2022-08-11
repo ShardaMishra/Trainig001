4adf272dea141e58bcfa9f37e45718d791afcb8f
Sharda Mishra <102789200+ShardaMishra@users.noreply.github.com>
2022-08-11 11:39:01 +0530

    changes done

--- filehandling/Demo.java
+++ filehandling/Demo.java
@@ -8,27 +8,35 @@ import java.io.IOException;
 
 public class Demo {
 	public static void main(String[] args) throws IOException {
-		BufferedWriter buffer = new BufferedWriter(
-				new FileWriter("C:\\Users\\Admin\\eclipse-workspace\\java training\\myfile.txt"));
-		/**
-		 * Using Write method to insert data into the file.
-		 */
-		buffer.write("hello");
-		buffer.write("\nFile has been created successfully");
+		BufferedWriter buffer = null;
+		BufferedReader bufferr = null;
+		try {
+			buffer = new BufferedWriter(
+					new FileWriter("C:\\Users\\Admin\\eclipse-workspace\\java training\\myfile.txt"));
+			/**
+			 * Using Write method to insert data into the file.
+			 */
+			buffer.write("hello");
+			buffer.write("\nFile has been created successfully");
+			bufferr = new BufferedReader(
+					new FileReader("C:\\Users\\Admin\\eclipse-workspace\\java training\\myfile.txt"));
+			/**
+			 * Using readLine() method to get the data present in the file.
+			 */
+			System.out.println(bufferr.readLine());
+			String Details = null;
+			while ((Details = bufferr.readLine()) != null) {
+				System.out.println(Details);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			buffer.close();
 
-		buffer.close();
-
-		BufferedReader bufferr = new BufferedReader(
-				new FileReader("C:\\Users\\Admin\\eclipse-workspace\\java training\\myfile.txt"));
-		/**
-		 * Using readLine() method to get the data present in the file.
-		 */
-		System.out.println(bufferr.readLine());
-		String Details = null;
-		while ((Details = bufferr.readLine()) != null) {
-			System.out.println(Details);
+			bufferr.close();
 		}
-		bufferr.close();
+
 	}
 
 }
