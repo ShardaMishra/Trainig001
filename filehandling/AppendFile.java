adccf6f5f799c51ff1ffa30c3de984cbf3970124
Sharda Mishra <102789200+ShardaMishra@users.noreply.github.com>
2022-08-11 11:30:13 +0530

    changes done

--- filehandling/AppendFile.java
+++ filehandling/AppendFile.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 
 public class AppendFile {
 	public static void main(String[] args) throws IOException {
+		BufferedReader bufferr1 = null;
 		try (FileWriter fw = new FileWriter("C:\\\\Users\\\\Admin\\\\eclipse-workspace\\\\java training\\\\myfile.txt",
 				true); BufferedWriter bw = new BufferedWriter(fw))
 
@@ -15,18 +16,19 @@ public class AppendFile {
 			bw.write("\n New Text Appended ");
 
 			bw.write("Appending successfull!!!");
+			bufferr1 = new BufferedReader(
+					new FileReader("C:\\\\Users\\\\Admin\\\\eclipse-workspace\\\\java training\\\\myfile.txt"));
 
+			System.out.println(bufferr1.readLine());
+			String Details = null;
+			while ((Details = bufferr1.readLine()) != null) {
+				System.out.println(Details);
+			}
 		} catch (IOException e) {
 			System.out.println("Invalid Input!!");
+		} finally {
+			bufferr1.close();
 		}
-		BufferedReader bufferr1 = new BufferedReader(
-				new FileReader("C:\\\\Users\\\\Admin\\\\eclipse-workspace\\\\java training\\\\myfile.txt"));
 
-		System.out.println(bufferr1.readLine());
-		String Details = null;
-		while ((Details = bufferr1.readLine()) != null) {
-			System.out.println(Details);
-		}
-		bufferr1.close();
 	}
 }
